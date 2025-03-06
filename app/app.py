f0b206b2d1c042811fa468342a97b2e0cbff7a7a
Cesar Caris <cesarantonioastete98@gmail.com>
2025-03-06 00:57:39 -0300

    [bug]Correccion de errores

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "¡Hola, DevOps con Docker y Kubernetes! esto es nuevo espero este todo bien configurado, asi solo tendre que pusherar las paginas para poder realizar los despliegues"
+    return "¡Hola, DevOps con Docker y Kubernetes! esto es nuevo espero este todo bien configurado, asi solo tendre que pusherar las paginas para poder realizar los despliegues (habian errores pero esta ves espero este todo correcto)"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
